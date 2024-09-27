7fb674f270d6e585e3658928ce65aa141b27304d
Kauã Albuquerque <kaualbuquerque44@gmail.com>
2024-09-26 21:09:58 -0300

    Teste de deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiKey: '2046dbb0d1ae41abb5a231007242008'  // Adicione sua chave da API aqui
+    API_KEY: '2046dbb0d1ae41abb5a231007242008'  // Adicione sua chave da API aqui
   };
   
