384ffa5786cdb0477f0df22ac96d1609bad26c10
Kauã Albuquerque <kaualbuquerque44@gmail.com>
2025-03-06 16:46:02 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    API_KEY: '2046dbb0d1ae41abb5a231007242008'  // Adicione sua chave da API aqui
+    production: true,
+    API_KEY: "API_KEY" 
   };
   
